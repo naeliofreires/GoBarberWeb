354bb816f39272527ed39d6f8f86a7e850445e15
naeliofreires <naeliofreires@gmail.com>
2019-10-17 10:36:20 -0300

    navegando entre dias

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,54 @@
-import React from 'react';
-import api from '~/services/api';
+import React, { useState, useMemo } from 'react';
 
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { format, subDays, addDays } from 'date-fns/esm';
+import { pt } from 'date-fns/esm/locale';
+
+import * as S from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
+  const [date, setDate] = useState(new Date());
 
-  return <h1>Dash</h1>;
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
+  return (
+    <S.Container>
+      <header>
+        <button type="button" onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <S.Time past>
+          <strong>08:00</strong>
+          <span>Naelio Freires</span>
+        </S.Time>
+        <S.Time>
+          <strong>09:00</strong>
+          <span>Narciso Freires</span>
+        </S.Time>
+        <S.Time available>
+          <strong>10:00</strong>
+          <span>Em aberto</span>
+        </S.Time>
+      </ul>
+    </S.Container>
+  );
 }
